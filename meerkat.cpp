69b88b72e33eda56e6930fdbbb4619cd1a72497e
stunlocke <mnorris@calpoly.edu>
2015-05-03 18:31:59 -0700

    Updated error checking.

--- meerkat.cpp
+++ meerkat.cpp
@@ -104,6 +104,7 @@ ports_set check_args(int argc, char **argv) {
   ports_set set(DEFAULT_PORT, DEFAULT_PORT);
   struct sockaddr_in sa;
   struct stat s;
+  bool p_set = false, b_set = false;
   char help[] = "-h";
   
   if (argc != HELP && argc != NO_PORTS && argc != ONE_PORT && argc != BOTH_PORTS) {
@@ -114,20 +115,47 @@ ports_set check_args(int argc, char **argv) {
   if (strcmp(argv[1], help) == 0) {
     cerr << "Usage: ./meerkat buddy_ipv4_address callback_binary_name " 
             "-p my_port -b buddy_port" << endl;
-    cerr << "(my_port and buddy_port signaled by -p and -b respectively)" << endl;
     exit(-1);
   }
   
   for (int i = 0; i < argc; ++i) {
     if (argv[i][0] == '-') {
-      if (argv[i][1] == 'p' && i+1 < argc) {
-        set.my_port = strtol(argv[i+1], NULL, 10);
+      if (argv[i][1] == 'p') {
+        if (i+1 < argc) {
+          if (p_set) {
+            cerr << "Self port entered more than once." << endl;
+            exit(-1);
+          }
+          else {
+            set.my_port = strtol(argv[i+1], NULL, 10);
+            p_set = true;
+          }
+        }
+        else {
+          cerr << "-p and -b should go before port number." << endl;
+          exit(-1);
+        }
       }
-      if (argv[i][1] == 'b' && i+1 < argc) {
-        set.buddy_port = strtol(argv[i+1], NULL, 10);
+      if (argv[i][1] == 'b') {
+        if (i+1 < argc) {
+          if (b_set) {
+            cerr << "Buddy port entered more than once." << endl;
+            exit(-1);
+          }
+          else {
+            set.buddy_port = strtol(argv[i+1], NULL, 10);
+            b_set = true;
+          }
+        }
+        else {
+          cerr << "-p and -b should go before port number." << endl;
+          exit(-1);
+        }
       }
     }
   }
+  
+  cerr << set.my_port << " " << set.buddy_port << endl;
   
   if (set.my_port < LOWEST_PORT || set.my_port > HIGHEST_PORT) {
     cerr << "Self port out of bounds (10k to 50k)." << endl;
@@ -150,8 +178,8 @@ ports_set check_args(int argc, char **argv) {
       cerr << "Callback expected binary (given a directory)." << endl;
       exit(-1);
     }
-    else if (s.st_mode & S_IFREG) {
-      // We're good, this is a file.
+    else if (s.st_mode & S_IFREG && (access(argv[2], X_OK)) == 0) {
+      // This is a file with execute permissions
     }
     else {
       // something else?
